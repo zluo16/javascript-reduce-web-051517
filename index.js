19b6f432273fe47037b4cbe33dfe0832d8f85960
zluo16 <zluo@alumni.berklee.edu>
2017-06-27 19:38:05 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
 const app = "I don't do much."
+
+const products = [
+  { name: 'Head & Shoulders Shampoo', price: 4.99, discount: .6 },
+  { name: 'Twinkies', price: 7.99, discount: .45 },
+  { name: 'Oreos', price: 6.49, discount: .8 },
+  { name: 'Jasmine-scented bath pearls', price: 13.99, discount: .7 },
+];
+
+// function getTotalAmountForProducts(products) {
+//   let totalPrice = 0;
+//
+//   products.forEach(product => {
+//     if (product.discount >= .5) {
+//       totalPrice += product.price;
+//     }
+//   })
+//   return totalPrice;
+// }
+
+// function getTotalAmountForProducts(products, callback) {
+//   let totalPrice = 0;
+//
+//   products.forEach(product => {
+//     totalPrice = callback(totalPrice, product);
+//   })
+//   return totalPrice;
+// }
+//
+// function callback(totalPrice, product) {
+//   if (product.price < 7) {
+//     return totalPrice + product.price;
+//   }
+//   return totalPrice;
+// }
+
+function reduce(collection, callback, initialValue) {
+  let result = initialValue;
+
+  collection.forEach(product => {
+    result = callback(result, product);
+  });
+
+  return result;
+}
+
+function callback(totalPrice, product) {
+  if (product.price < 7) {
+    return totalPrice + product.price;
+  }
+  return totalPrice;
+}
